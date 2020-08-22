1bee6a4db3d284f3bccd38b2930579b2534d278d
Kekoa Kubli <kkubli3246@gmail.com>
2020-08-22 14:24:04 -0700

    first commit

---
+++ Controllers/CustomerController.cs
@@ -0,0 +1,64 @@
+﻿using Activity3.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.Mvc;
+
+namespace Activity3.Controllers
+{
+
+
+    public class CustomerController : Controller
+    {
+        CustomerModel c1 = new CustomerModel("Jeff", 14);
+        CustomerModel c2 = new CustomerModel("Jen", 40);
+        CustomerModel c3 = new CustomerModel("Sum", 125);
+
+        public List<CustomerModel> customer = new List<CustomerModel>();
+
+       
+
+        // GET: Customer
+        public ActionResult Index()
+        {
+
+            customer.Add(c1);
+            customer.Add(c2);
+            customer.Add(c3);
+
+            foreach (var c in customer) 
+            {
+                c.ID = customer.IndexOf(c);
+            }
+
+            Tuple<List<CustomerModel>, CustomerModel> tuple  = new Tuple<List<CustomerModel>, CustomerModel>(customer, customer[0]);
+
+            
+            return View("Customer", tuple);
+        }
+
+        
+        public ActionResult OnSelectCustomer(string cus)
+        {
+            customer.Add(c1);
+            customer.Add(c2);
+            customer.Add(c3);
+            foreach (var c in customer)
+            {
+                c.ID = customer.IndexOf(c);
+            }
+
+
+            //Tuple<List<CustomerModel>, CustomerModel> tuple = new Tuple<List<CustomerModel>, CustomerModel>(customer, customer[Int32.Parse(cus)]);
+
+            //return View("Customer", tuple);
+            return PartialView("_CustomerDetails", customer[Int32.Parse(cus)]);
+      
+        }
+    }
+
+    
+}
---
+++ Models/CustomerModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Activity3.Models
+{
+    public class CustomerModel : IComparable
+    {
+        public int ID { get; set; }
+
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+
+        [Display(Name = "Age")]
+        public int Age { get; set; }
+
+        public CustomerModel(string name, int age)
+        {
+            
+            this.Name = name;
+            this.Age = age;
+
+        }
+
+        public int CompareTo(object obj)
+        {
+            CustomerModel cus = obj as CustomerModel;
+
+            return Name.CompareTo(cus.Name);
+        }
+    }
+}
